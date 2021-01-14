81ebd354cc73f4a2c83e160a66ad8765272107df
iamlalit <iamlalit.sachdeva@gmail.com>
2021-01-14 15:03:55 +0530

    updated for undefined

--- index.js
+++ index.js
@@ -31,8 +31,11 @@ bot.on('message', (data) => {
     if(data.type !== 'message' || data.subtype === 'bot_message') {
         return;
     }
-    if(data.channel == BOT_CHANNEL)
-        handleMessage(data.text, data.channel, data.user);
+    //console.log(data.channel);
+    if(typeof data.text !== "undefined"){
+        if(data.channel == BOT_CHANNEL){
+            handleMessage(data.text, data.channel, data.user);
+        }
     }
         
 })
